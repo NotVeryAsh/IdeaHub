d8c91b370a5d973e31e8bd76579c2ef5687a2318
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-08-02 20:11:14 +0100

    Add basis of user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'username',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
