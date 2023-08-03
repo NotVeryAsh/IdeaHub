a96073d52f5a087c986aec2416ffe4e3a7a2ae33
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-08-03 13:30:53 +0100

    Add controller and routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Docs\Architecture\HttpVerbsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::prefix('docs')->group(function () {
+    Route::prefix('architecture')->group(function () {
+        Route::get('http-verbs', [HttpVerbsController::class, 'index']);
+    });
+});
