ce5373b1dca3889a1f533b51367587204d3733a5
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-08-03 13:29:45 +0100

    Add test to see if all blade files are included

--- tests/Feature/Docs/Architecture/HttpVerbsTest.php
+++ tests/Feature/Docs/Architecture/HttpVerbsTest.php
@@ -6,11 +6,24 @@ use Tests\TestCase;
 
 class HttpVerbsTest extends TestCase
 {
-    public function test_it_can_access_http_verbs_page()
+    public function test_can_access_http_verbs_page()
     {
         $response = $this->get('/docs/architecture/http-verbs');
 
         $response->assertStatus(200);
         $response->assertViewIs('docs.architecture.http-verbs.index');
+    }
+
+    public function test_http_verb_blade_files_are_included()
+    {
+        $response = $this->get('/docs/architecture/http-verbs');
+
+        $response->assertSeeInOrder([
+            '<!-- GET Verb -->',
+            '<!-- POST Verb -->',
+            '<!-- PUT Verb -->',
+            '<!-- PATCH Verb -->',
+            '<!-- DELETE Verb -->',
+        ]);
     }
 }
