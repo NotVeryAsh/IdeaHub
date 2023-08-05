7c05060a301e1aa8f800789af5bb5b075b5e7bc3
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-08-05 15:38:40 +0100

    Add basis for views (functional but awful looking

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,7 @@
+<form action="/auth/login" method="post">
+    @csrf
+
+    <input type="text" name="identifier" placeholder="Email or username" maxlength="255" required>
+    <input type="password" name="password" placeholder="Password" required maxlength="60">
+    <button type="submit">Login</button>
+</form>
