ae9fd98c5b19558149eda568c30a09a8d5a4be29
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-08-13 10:11:00 +0100

    Add logout route

--- routes/auth.php
+++ routes/auth.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-// Email verification routes
+// Email verification and logout routes
 Route::prefix('auth')->group(function () {
     Route::prefix('verify-email')->middleware('auth')->group(function () {
 
@@ -13,6 +14,8 @@ Route::prefix('auth')->group(function () {
         Route::post('resend', [EmailVerificationController::class, 'resend'])->middleware(['throttle:6,1'])->name('verification.send');
         Route::get('{id}/{hash}', [EmailVerificationController::class, 'verify'])->middleware(['signed'])->name('verification.verify');
     });
+
+    Route::post('logout', LogoutController::class)->name('logout');
 });
 
 // Login and dashboard routes
@@ -21,11 +24,11 @@ Route::middleware('guest')->group(function () {
     // Authentication routes
     Route::prefix('auth')->group(function () {
 
-        Route::post('login', [LoginController::class, 'authenticate'])->middleware('guest');
-        Route::post('register', [RegisterController::class, 'authenticate'])->middleware('guest');
+        Route::post('login', [LoginController::class, 'authenticate']);
+        Route::post('register', [RegisterController::class, 'authenticate']);
     });
 
     // Index routes for views
-    Route::get('login', [LoginController::class, 'index'])->middleware('guest')->name('login');
-    Route::get('register', [RegisterController::class, 'index'])->middleware('guest')->name('register');
+    Route::get('login', [LoginController::class, 'index'])->name('login');
+    Route::get('register', [RegisterController::class, 'index'])->name('register');
 });
