7a6e3e4f32e1b6996a6ebe7f3c4a1f766bed49d0
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-08-13 12:05:54 +0100

    Add basis of forgot password tests, add reset password traits and interface to user model, add reset password migration and enable trust hosts middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Passwords\CanResetPassword as CanResetPasswordTrait;
+use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail, CanResetPassword
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, CanResetPasswordTrait;
 
     protected $fillable = [
         'first_name',
--- tests/Feature/Auth/ForgotPasswordTest.php
+++ tests/Feature/Auth/ForgotPasswordTest.php
@@ -6,4 +6,13 @@ use Tests\TestCase;
 
 class ForgotPasswordTest extends TestCase
 {
+    public function test_can_send_forgot_password_email()
+    {
+
+    }
+
+    public function test_can_reset_password()
+    {
+
+    }
 }
