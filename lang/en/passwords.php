c1e06b5dbf2391909095837364a2b20223dd7beb
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-08-13 12:36:53 +0100

    Adjust forgot email messages

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
+    'reset' => 'Password has been reset.',
+    'sent' => 'Password reset email sent.',
     'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'token' => 'Password reset token is invalid.',
+    'user' => 'Email Address is invalid.',
 
 ];
