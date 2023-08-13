3f0067d9a90064d494f68d67592d6fc3176a7cd3
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-08-13 12:35:53 +0100

    Add Laravel functionality to send forgot password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\ForgotPasswordRequest;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
 
 class ForgotPasswordController extends Controller
@@ -16,8 +18,19 @@ class ForgotPasswordController extends Controller
         return view('auth.forgot-password');
     }
 
-    public function sendNotification()
+    public function sendResetLinkNotification(ForgotPasswordRequest $request)
     {
+        // Send password reset link to user
+        $status = Password::sendResetLink($request->validated('email'));
 
+        $statusMessage = __($status);
+
+        // Sending password reset notification was not successful
+        if ($status !== Password::RESET_LINK_SENT) {
+            return back()->withErrors(['email' => $statusMessage]);
+        }
+
+        // Sending password reset notification was successful
+        return back()->with(['status' => $statusMessage]);
     }
 }
