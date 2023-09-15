4d3a1266ee4dfdc802aec4ac1ed8a4faa3dbbbf0
Ash <ashley.phillips@fatjoe.com>
2023-09-15 14:22:26 +0100

    Fix factory

--- database/factories/DefaultProfilePictureFactory.php
+++ database/factories/DefaultProfilePictureFactory.php
@@ -28,18 +28,11 @@ class DefaultProfilePictureFactory extends Factory
     {
         return $this->afterMaking(function (DefaultProfilePicture $profilePicture) {
 
-            $path = $profilePicture->path;
-
-            // If profile picture has not been set
-            if(!$path) {
-                return;
-            }
-
             // Fake create profile picture based on the path
             $image = fake()->image(null, 100, 100);
 
             // Store profile picture in storage
-            Storage::put($path, $image);
+            Storage::put($profilePicture->path, $image);
         });
     }
 }
