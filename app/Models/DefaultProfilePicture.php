1c22c337f1a24beef14be91fde9c8224d4688851
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-09-28 13:06:43 +0100

    stop model from using authenticatable when it doesn't need to be

--- app/Models/DefaultProfilePicture.php
+++ app/Models/DefaultProfilePicture.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class DefaultProfilePicture extends Authenticatable
+class DefaultProfilePicture extends Model
 {
     use HasFactory;
 
