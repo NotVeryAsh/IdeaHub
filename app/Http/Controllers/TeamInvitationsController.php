669909e99d779d14305b33853a14574486af44e9
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-10-05 10:45:54 +0100

    Edit mailable and mail content, and adjust functionality to store invitation

--- app/Http/Controllers/TeamInvitationsController.php
+++ app/Http/Controllers/TeamInvitationsController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Teams\StoreTeamInvitationRequest;
+use App\Mail\Invitations\TeamInvitationSent;
 use App\Models\Team;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 
 class TeamInvitationsController extends Controller
 {
-    public function store(StoreTeamInvitationRequest $request, Team $team)
+    public function store(StoreTeamInvitationRequest $request, Team $team): RedirectResponse
     {
         // Get the recipient email from the request
         $recipient = $request->validated('email');
@@ -18,7 +20,7 @@ class TeamInvitationsController extends Controller
         $url = URL::temporarySignedRoute('invitations.accept', now()->addWeek(), ['email' => $recipient, 'team' => $team]);
 
         // Send a mailable to the recipient with the signed url and the team
-        Mail::to($recipient)->queue(new TeamInvitationSent($team, $url));
+        Mail::to($request)->queue(new TeamInvitationSent($team, $url));
 
         // Redirect back to the team page with a success message
         return redirect()->route('teams.show', $team)->with(['status' => 'Invitation sent!']);
--- app/Mail/Invitations/TeamInvitationSent.php
+++ app/Mail/Invitations/TeamInvitationSent.php
@@ -10,21 +10,21 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class InvitationSent extends Mailable
+class TeamInvitationSent extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public String $recipientEmail;
-
     public Team $team;
+
+    public string $url;
 
     /**
      * Create a new message instance.
      */
-    public function __construct($recipientEmail, $team)
+    public function __construct($team, $url)
     {
-        $this->$recipientEmail = $recipientEmail;
         $this->team = $team;
+        $this->url = $url;
     }
 
     /**
@@ -34,7 +34,7 @@ class InvitationSent extends Mailable
     {
         return new Envelope(
             from: new Address(config('mail.from.address'), config('mail.from.name')),
-            subject: "{$this->team->name} Team Created!",
+            subject: "You have been invited to join the {$this->team->name} team!",
         );
     }
 
@@ -42,13 +42,12 @@ class InvitationSent extends Mailable
      * Get the message content definition.
      */
     public function content(): Content
-    { // TODO Change members page url
+    {
         return new Content(
-            markdown: 'emails.teams.created',
+            markdown: 'emails.invitations.sent',
             with: [
-                'user' => $this->user,
                 'team' => $this->team,
-                'membersPageURL' => /*route('teams.members', $this->team)*/ config('app.url')."/teams/{$this->team->id}/members",
+                'url' => $this->url,
             ],
         );
     }
