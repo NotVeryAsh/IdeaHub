705143482b5fcb2b826e63d8d85dda7a11841c6f
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-10-28 08:27:24 +0100

    Add basis for tests

---
+++ tests/Feature/Teams/RestoreTeamTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Teams;
+
+use Tests\TestCase;
+
+class RestoreTeamTest extends TestCase
+{
+    public function test_can_restore_team()
+    {
+
+    }
+
+    public function test_cannot_restore_a_team_that_is_not_soft_deleted()
+    {
+
+    }
+
+    public function test_only_team_creator_can_restore_team()
+    {
+
+    }
+
+    public function test_authentication_is_required_to_restore_team()
+    {
+
+    }
+
+    public function test_404_returned_when_team_is_not_found()
+    {
+
+    }
+}
