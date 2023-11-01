2a8d6b5105123c7b9019a45d07f6c33f553ddbda
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-11-01 21:15:19 +0000

    Fix future hashing issues with laravel update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -25,7 +26,7 @@ class UserFactory extends Factory
             'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
             'username_updated_at' => now()->subHours(7),
             'profile_picture' => null,
